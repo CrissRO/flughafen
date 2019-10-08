94d9e5caf7e7c764b4a9649ca879ea8c148ac5b7
CrissRO <cezarmocanu2014@gmail.com>
2019-10-08 19:21:13 +0200

    [cmocanu] Fluglinie modifications

--- src/de/hhn/se/gs2/flugzeuge/model/Flug.java
+++ src/de/hhn/se/gs2/flugzeuge/model/Flug.java
@@ -1,6 +1,8 @@
 package de.hhn.se.gs2.flugzeuge.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author (cmocanu)Cezar-Cristian Mocanu
@@ -8,23 +10,50 @@ import java.time.LocalDateTime;
 
 public class Flug {
 
+	//Die final Variablen sind mit grossen Buchstaben geschrieben 
 	private final LocalDateTime DATE;
 	private final int FLUG_NUMMER;
+	private Fluglinie anbieter;
+	private Flugzeug verkehrsMittel;
 	
+	private List<Flughafen> ziele;
+	private List<Flughafen> herkunfte;
+	private List<Pilot> flugzeugBesazung;//flight crew
+	private List<Passagier> flugReisender;
+	
+
 	public Flug(LocalDateTime date,int flugNummer) {
-		this.DATE = date;
-		this.FLUG_NUMMER = flugNummer;
+		DATE = date;
+		FLUG_NUMMER = flugNummer;
+		ziele = new ArrayList<>();
+		herkunfte = new ArrayList<>();
+		flugzeugBesazung = new ArrayList<>();
+		flugReisender = new ArrayList<>();
 	}
 
-	public void einstellen() {}
+	public void einstellen() {
+		System.out.println("Der Flug #"+ FLUG_NUMMER+" ist eingestellt");
+	}
 	
-	public void abheben() {}
+	public void reservieren() {
+		System.out.println("Sie haben der Flug #"+ FLUG_NUMMER+" reserviert");
+	}
 	
-	public void landen() {}
+	public void abheben() {
+		System.out.println("Der Flug #"+ FLUG_NUMMER+" hat abgehoben");
+	}
 	
-	public void streichen() {}
+	public void landen() {
+		System.out.println("Der Flug #"+ FLUG_NUMMER+" ist gelandet");
+	}
 	
-	public void verspatten() {}
+	public void streichen() {
+		System.out.println("Der Flug #"+ FLUG_NUMMER+" ist gestrichen");
+	}
+	
+	public void verspatten() {
+		System.out.println("Flug #"+ FLUG_NUMMER+" verspaetet");
+	}
 	
 	public LocalDateTime getDATE() {
 		return DATE;
