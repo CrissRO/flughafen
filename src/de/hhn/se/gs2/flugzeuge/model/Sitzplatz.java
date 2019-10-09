fb66aa8dc6236135b82dec1e2f15262f12fbf5a9
GiuliaR11 <radugiulia@yahoo.com>
2019-10-09 12:13:43 +0200

    [gradu] Bordkarte,Passagier,Sitzplatz

--- src/de/hhn/se/gs2/flugzeuge/model/Sitzplatz.java
+++ src/de/hhn/se/gs2/flugzeuge/model/Sitzplatz.java
@@ -1,4 +1,8 @@
 package de.hhn.se.gs2.flugzeuge.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * @author Radu Giulia
  * */
@@ -7,12 +11,31 @@ public class Sitzplatz {
 	private final String ORT;
 	private final int  REIHE;
 	private final char  PLATZ;
+	private final Flugzeug FLUGZEUG ;
+	private List<Bordkarte> tickets;
 
 	
-	public Sitzplatz(String ort, int reihe, char platz) {
+	public Sitzplatz(String ort, int reihe, char platz,Flugzeug flugzeug) {
 		ORT = ort;
 		REIHE = reihe;
 		PLATZ = platz;
+		FLUGZEUG = flugzeug;
+		this.tickets = new ArrayList<Bordkarte>();
+	}
+	
+	public void addBordkarte(Bordkarte b) throws Exception {
+		if(!tickets.contains(b))
+			tickets.add(b);
+		else
+			throw new Exception("Die Bordkarte existiert bereits");
+	}
+
+	public List<Bordkarte> getTickets() {
+		return tickets;
+	}
+
+	public void setTickets(List<Bordkarte> tickets) {
+		this.tickets = tickets;
 	}
 
 	public void reservieren() {
